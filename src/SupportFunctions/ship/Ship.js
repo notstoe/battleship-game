36f02d8787a24ce32d38a33e150e950d2acac208
notstoe <gstonin95@gmail.com>
2021-04-13 18:27:29 +0100

    testing ship object, taking hits and returning correct object

---
+++ src/SupportFunctions/ship/Ship.js
@@ -0,0 +1,34 @@
+export default function Ship(length) {
+	let shipBlocks = [];
+
+	for (let i = 0; i < length; i++) {
+		shipBlocks.push({ isHit: false });
+	}
+
+	function sinkShip() {
+		const sunkShip = { ...ship, isSunk: true };
+
+		return sunkShip;
+	}
+
+	function takeHit(index) {
+		let dmgedShip = { ...ship };
+
+		dmgedShip.shipBlocks[index].isHit = true;
+
+		const checkDmg = dmgedShip.shipBlocks.filter((block) => block.isHit); //check if all blocks are hit or not
+		if (checkDmg.length === dmgedShip.shipBlocks.length) {
+			return sinkShip();
+		} else {
+			return dmgedShip;
+		}
+	}
+
+	const ship = {
+		shipBlocks: shipBlocks,
+		isSunk: false,
+		takeHit: takeHit,
+	};
+
+	return ship;
+}
---
+++ src/tests/ship/Ship.test.js
@@ -0,0 +1,59 @@
+import Ship from "../../SupportFunctions/ship/Ship";
+
+it("returns ship object with correct structure", () => {
+	expect(Ship(1)).toMatchObject({
+		shipBlocks: [{ isHit: false }],
+		isSunk: false,
+		takeHit: expect.any(Function),
+	});
+});
+
+it("returns ship object with correct length", () => {
+	expect(Ship(3)).toEqual({
+		shipBlocks: [{ isHit: false }, { isHit: false }, { isHit: false }],
+		isSunk: false,
+		takeHit: expect.any(Function),
+	});
+});
+
+describe("testing dmg to a ship of length 3", () => {
+	let destroyer;
+	let fakeState;
+
+	beforeEach(() => {
+		destroyer = Ship(3);
+	});
+
+	it("returns new ship with target block hit", () => {
+		expect(destroyer.takeHit(0)).toEqual({
+			shipBlocks: [{ isHit: true }, { isHit: false }, { isHit: false }],
+			isSunk: false,
+			takeHit: expect.any(Function),
+		});
+	});
+
+	it("testing assigning to fakeState with block hit", () => {
+		fakeState = destroyer.takeHit(0);
+		expect(fakeState).toEqual({
+			shipBlocks: [{ isHit: true }, { isHit: false }, { isHit: false }],
+			isSunk: false,
+			takeHit: expect.any(Function),
+		});
+	});
+
+	it("returns already dmged ship with target block hit", () => {
+		expect(fakeState.takeHit(2)).toEqual({
+			shipBlocks: [{ isHit: true }, { isHit: false }, { isHit: true }],
+			isSunk: false,
+			takeHit: expect.any(Function),
+		});
+	});
+
+	it("returns a sunkShip with all blocks hit", () => {
+		expect(fakeState.takeHit(1)).toEqual({
+			shipBlocks: [{ isHit: true }, { isHit: true }, { isHit: true }],
+			isSunk: true,
+			takeHit: expect.any(Function),
+		});
+	});
+});
