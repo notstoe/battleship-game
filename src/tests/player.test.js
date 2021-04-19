6bc12dbe6ee3e0223950249a491badb8a48f5050
notstoe <gstonin95@gmail.com>
2021-04-19 21:29:34 +0100

    testing player functions for getName, setName, getScore and incrementScore

---
+++ src/tests/player.test.js
@@ -0,0 +1,33 @@
+import player from "../factories/player";
+
+describe("player() factory Usage", () => {
+	const player1 = player("Gustavo");
+
+	it("player returns object matching such structure", () => {
+		expect(player1).toMatchObject({
+			getScore: expect.any(Function),
+			getName: expect.any(Function),
+			incrementScore: expect.any(Function),
+			setName: expect.any(Function),
+		});
+	});
+
+	it("player getScore returns 0 initially", () => {
+		expect(player1.getScore()).toBe(0);
+	});
+
+	it("player incrementScore returns 1 after won game", () => {
+		player1.incrementScore();
+		expect(player1.getScore()).toBe(1);
+	});
+
+	it("player getName returns player's name", () => {
+		expect(player1.getName()).toMatch("Gustavo");
+	});
+
+	it("player setName changes player's name", () => {
+		player1.setName("Tonin");
+
+		expect(player1.getName()).toMatch("Tonin");
+	});
+});
