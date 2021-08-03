0e2500219fdaf832d2257fe97f79e0c0a7545dbf
notstoe <gstonin95@gmail.com>
2021-08-03 18:10:31 +0100

    api keys hard coded into file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const appF = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: "AIzaSyDPn8IA_FhKnY2MNa9K-_MqUdWUPXpkLE8",
+	authDomain: "battleship-d0a52.firebaseapp.com",
+	projectId: "battleship-d0a52",
+	storageBucket: "battleship-d0a52.appspot.com",
+	messagingSenderId: "battleship-d0a52.appspot.com",
+	appId: "1:596484871324:web:4e4e7c7768202a1ea78959",
+	measurementId: "G-C0EYKMYY4J",
 });
 export const auth = appF.auth();
 export default appF;
